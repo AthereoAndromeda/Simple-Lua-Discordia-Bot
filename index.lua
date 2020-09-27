c95781010bd72db88fbb39a099a722224a227ad1
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-09-27 21:46:34 +0800

    Update index.lua

--- index.lua
+++ index.lua
@@ -12,7 +12,7 @@ Discordia.extensions()
 table.filter = function(t, filterIter)
     local out = {}
     for k, v in ipairs(t) do
-      if filterIter(v, k, t) then table.insert(out, v) end
+        if filterIter(v, k, t) then table.insert(out, v) end
     end
     return out
 end
