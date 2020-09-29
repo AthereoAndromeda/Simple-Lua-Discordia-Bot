dd1e70e758a2042aa49e73ccae4ba3e0ff1aff06
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-09-29 21:13:41 +0800

    Update dadjoke.lua

--- commands/dadjoke.lua
+++ commands/dadjoke.lua
@@ -6,17 +6,15 @@ return {
         local http = require("coro-http")
 
         local status, err = pcall(function()
-            coroutine.wrap(function()
-                local link = "https://icanhazdadjoke.com/"
-                local headers = {
-                    {"Accept", "application/json"}
-                }
-    
-                local res, body = http.request("GET", link, headers)
-                local file = JSON.parse(body)
-                --if file.status < 200 or file.status >= 300 then error("Status Code: "..file.status) end
-                return message.channel:send(file.joke)
-            end)()
+            local link = "https://icanhazdadjoke.com/"
+            local headers = {
+                {"Accept", "application/json"}
+            }
+
+            local res, body = http.request("GET", link, headers)
+            local file = JSON.parse(body)
+            --if file.status < 200 or file.status >= 300 then error("Status Code: "..file.status) end
+            return message.channel:send(file.joke)
         end)
 
         if not status then
