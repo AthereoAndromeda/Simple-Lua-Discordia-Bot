d985e5a4aa386878e3cbbc49e4ed8a0e3a426d04
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-09-29 21:12:18 +0800

    Update fact.lua

--- commands/fact.lua
+++ commands/fact.lua
@@ -12,24 +12,22 @@ return {
         local http = require("coro-http")
 
         local status, err = pcall(function()
-            coroutine.wrap(function()
-                local randomLink = "https://uselessfacts.jsph.pl/random.json?language=en"
-                local todayLink = "https://uselessfacts.jsph.pl/today.json?language=en"
+            local randomLink = "https://uselessfacts.jsph.pl/random.json?language=en"
+            local todayLink = "https://uselessfacts.jsph.pl/today.json?language=en"
 
-                local options = {
-                    headers = {
-                        {"Accept", "application/json"},
-                    }
+            local options = {
+                headers = {
+                    {"Accept", "application/json"},
                 }
+            }
 
-                if not args then
-                    res, body = http.request("GET", randomLink, options.headers)
-                elseif args:lower() == "today" then
-                    res, body = http.request("GET", todayLink, options.headers)
-                end
-                local file = JSON.parse(body)
-                message:reply(file.text)
-            end)()
+            if not args then
+                res, body = http.request("GET", randomLink, options.headers)
+            elseif args:lower() == "today" then
+                res, body = http.request("GET", todayLink, options.headers)
+            end
+            local file = JSON.parse(body)
+            message:reply(file.text)
         end)
 
         if not status then
