972c9234a0c2a9268b58c2421dcbad91e8c9e8a6
Angelo <70970878+redstripez08@users.noreply.github.com>
2020-09-29 21:28:34 +0800

    Add files via upload

---
+++ package.lua
@@ -0,0 +1,27 @@
+  return {
+    name = "Simple-Lua-Discordia-Bot",
+    version = "1.1.5",
+    description = "Just A simple Lua Discordia Bot",
+    tags = { "lua", "lit", "luvit" },
+    license = "MIT",
+    author = { name = "Angelo", email = "nil" },
+    homepage = "https://github.com/Simple-Lua-Discordia-Bot",
+    dependencies = {
+      "SinisterRectus/discordia"
+    },
+    files = {
+      "index.lua",
+      "config.json",
+      "discordia.log",
+      "gateway.json",
+      "cat.lua",
+      "dadjoke.lua",
+      "dog.lua",
+      "embed.lua",
+      "fact.lua",
+      "help.lua",
+      "test.lua",
+      "thing.lua"
+    }
+  }
+  
